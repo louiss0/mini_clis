302eec54206415c3615f343d73d9a7a3a545c357
louiss0 <louisshelton0@gmail.com>
2025-02-13 10:19:58 -0500

    feat: return error when an priority or completion flag is passed with wrong argument When the priority isn't high,low or medium when the priority flag is passed an error is returned.  When the flag is completion and the value isn't either complete or incomplete then an error is returned. Both errors are called invalid argument errors I decided to also set all the flags to mutually exclusive since they simply are there to change the behavior of delete

--- task-list/cmd/delete.go
+++ task-list/cmd/delete.go
@@ -4,13 +4,22 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/mini-clis/task-list/custom_errors"
 	"github.com/mini-clis/task-list/task"
 	"github.com/samber/lo"
 	"github.com/spf13/cobra"
 )
+
+const COMPLETION = "completion"
+
+var allowedCompletionValues = []string{
+	"complete",
+	"incomplete",
+}
 
 // deleteCmd represents the delete command
 func CreateDeleteCommand() *cobra.Command {
@@ -21,7 +30,8 @@ func CreateDeleteCommand() *cobra.Command {
 			The flags in this command allow you to pass in
 			a title or delete tasks with specific properties.
 		`,
-		Args: cobra.ExactArgs(1),
+		SilenceUsage: true,
+		Args:         cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 
 			tasks, error := task.ReadTasks()
@@ -31,6 +41,41 @@ func CreateDeleteCommand() *cobra.Command {
 			}
 
 			firstArgument := args[0]
+
+			_, titleError := cmd.Flags().GetBool(TITLE)
+			completion, completionError := cmd.Flags().GetBool(COMPLETION)
+			priority, priorityError := cmd.Flags().GetBool(PRIORITY)
+
+			flagErrors := errors.Join(titleError, completionError, priorityError)
+
+			if flagErrors != nil {
+
+				return custom_errors.CreateInvalidFlagErrorWithMessage(
+					flagErrors.Error(),
+				)
+			}
+
+			if completion && !lo.Contains(allowedCompletionValues, firstArgument) {
+
+				return custom_errors.CreateInvalidArgumentErrorWithMessage(
+					fmt.Sprintf(
+						"When you use the %s flag you must pass in the %s",
+						COMPLETION,
+						strings.Join(allowedCompletionValues, ","),
+					),
+				)
+			}
+
+			if priority && !lo.Contains(task.AllowedProrities, firstArgument) {
+
+				return custom_errors.CreateInvalidArgumentErrorWithMessage(
+					fmt.Sprintf(
+						"When you use the %s flag you must pass in the %s",
+						PRIORITY,
+						strings.Join(task.AllowedProrities, ","),
+					),
+				)
+			}
 
 			filteredTasks := lo.Filter(tasks, func(item task.Task, index int) bool {
 				return item.Id() != firstArgument
@@ -57,6 +102,23 @@ func CreateDeleteCommand() *cobra.Command {
 			return nil
 		},
 	}
+
+	allowedFlagNames := []string{PRIORITY, COMPLETION, TITLE}
+
+	deleteCmdFlags := deleteCmd.Flags()
+
+	lo.ForEach(allowedFlagNames, func(item string, index int) {
+
+		deleteCmdFlags.Bool(
+			item,
+			false,
+			fmt.Sprintf("Delete tasks based on %s", item),
+		)
+
+	})
+
+	deleteCmd.MarkFlagsMutuallyExclusive(allowedFlagNames...)
+
 	return deleteCmd
 }
 
