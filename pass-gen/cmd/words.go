078f44d8b61854d674137571fe96233de1b1c2d2
louiss0 <louisshelton0@gmail.com>
2025-02-19 17:15:01 -0500

    feat(words): create words command

--- pass-gen/cmd/words.go
+++ pass-gen/cmd/words.go
@@ -22,29 +22,33 @@ THE SOFTWARE.
 package cmd
 
 import (
-	"fmt"
-
+	"github.com/mini-clis/pass-gen/printer"
 	"github.com/spf13/cobra"
 )
 
 // wordsCmd represents the words command
-var wordsCmd = &cobra.Command{
-	Use:   "words",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
+func CreateWordsCmd() *cobra.Command {
+	var wordsCmd = &cobra.Command{
+		Use:   "words",
+		Short: "A brief description of your command",
+		Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Args: cobra.NoArgs,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("words called")
-	},
-}
+		Args: cobra.NoArgs,
 
+		RunE: func(cmd *cobra.Command, args []string) error {
+
+			return printer.PrintUsingCommmand(cmd, "foo")
+		},
+	}
+
+	return wordsCmd
+}
 func init() {
-	rootCmd.AddCommand(wordsCmd)
+	rootCmd.AddCommand(CreateWordsCmd())
 
 	// Here you will define your flags and configuration settings.
 
