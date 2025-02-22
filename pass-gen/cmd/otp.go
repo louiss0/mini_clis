463cd8d174b02fdde1e3e95d8342dc0242c61b3e
louiss0 <louisshelton0@gmail.com>
2025-02-21 21:04:43 -0500

    feat: create otp command

---
+++ pass-gen/cmd/otp.go
@@ -0,0 +1,70 @@
+/*
+Copyright © 2025 Shelton Louis
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package cmd
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/mini-clis/pass-gen/printer"
+	"github.com/spf13/cobra"
+	"github.com/xlzd/gotp"
+)
+
+func NewOTPCmd() *cobra.Command {
+	otpCmd := &cobra.Command{
+		Use:   "otp",
+		Short: "Generate a one-time password",
+		Args:  cobra.ExactArgs(1),
+		Long:  `Generate a one-time password using the HOTP or TOTP algorithm.`,
+		Run: func(cmd *cobra.Command, args []string) {
+
+			length, err := strconv.Atoi(args[0])
+			if err != nil {
+				fmt.Println("Invalid length")
+				return
+			}
+
+			secretKey := gotp.RandomSecret(length)
+
+			totp := gotp.NewDefaultTOTP(secretKey)
+
+			printer.PrintUsingCommmand(cmd, totp.Now())
+		},
+	}
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// otpCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// otpCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	return otpCmd
+}
+
+func init() {
+	rootCmd.AddCommand(NewOTPCmd())
+}
