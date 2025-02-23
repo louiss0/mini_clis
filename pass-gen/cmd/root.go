2d44531af54b398041d4b3515b9e8c9e7b19c699
louiss0 <louisshelton0@gmail.com>
2025-02-22 22:19:42 -0500

    refactor(root): add better description of this CLI

--- pass-gen/cmd/root.go
+++ pass-gen/cmd/root.go
@@ -31,14 +31,17 @@ import (
 
 var rootCmd = &cobra.Command{
 	Use:   "pass-gen",
-	Short: "A brief description of your application",
+	Short: "Generate random passwords",
 	Args:  cobra.NoArgs,
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long: `This application is a tool to generate random passwords.
+	You can generate many kinds of passwords,
+	including but not limited to:
+	- Passwords with numbers
+	- Passwords with special characters
+	- Passwords with uppercase letters
+	- Passwords with lowercase letters
+	- Passwords with a mix of all the above
+	`,
 	SilenceUsage: true,
 }
 
