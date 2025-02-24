f904abb2ba1a96d56395133c0c3425128d57a1b3
louiss0 <louisshelton0@gmail.com>
2025-02-24 14:05:40 -0500

    feat: create encode command This is the creation of a new encode command This command takes in any amount of arguments and encodes them into a string. The user must supply at least one argument. So far it combines all decodes strings into one.

---
+++ pass-gen/cmd/encode.go
@@ -0,0 +1,68 @@
+/*
+Copyright © 2025 Shelton Louis
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package cmd
+
+import (
+	"encoding/hex"
+	"strings"
+
+	"github.com/mini-clis/pass-gen/printer"
+	"github.com/samber/lo"
+	"github.com/spf13/cobra"
+)
+
+// CreateEncodeCmd creates and returns the encode command
+func CreateEncodeCmd() *cobra.Command {
+	encodeCmd := &cobra.Command{
+		Use:   "encode",
+		Short: "Encode a string",
+		Long: `Encode a series of characters into a base64 encoded string.
+		You can only use letters, numbers, and symbols.
+		Spaces are not allowed.`,
+		Args: cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+
+			hexcodedArgs := lo.Map(args, func(arg string, _ int) string {
+				return hex.EncodeToString([]byte(arg))
+			})
+
+
+			printer.PrintUsingCommmand(cmd, strings.Join(hexcodedArgs, ""))
+
+		},
+	}
+	return encodeCmd
+}
+
+func init() {
+	rootCmd.AddCommand(CreateEncodeCmd())
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// encodeCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// encodeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
