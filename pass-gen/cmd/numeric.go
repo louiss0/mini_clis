01978400026349ae5992d53dd9d6fe38faa7bd8b
louiss0 <louisshelton0@gmail.com>
2025-02-24 11:22:30 -0500

    feat(numeric): create numeric timestamp using a flag The user can now create a timestamp by using the date-pin flag. The length and date-pin flag are mutually exclusive. This makes it so that the user can't make specify al length and date-pin. An error is now retuned when the date-pin flag is invalid.

--- pass-gen/cmd/numeric.go
+++ pass-gen/cmd/numeric.go
@@ -26,8 +26,10 @@ import (
 	"fmt"
 	"math/big"
 	"strconv"
+	"time"
 
 	"github.com/mini-clis/pass-gen/printer"
+	"github.com/mini-clis/shared/custom_errors"
 	"github.com/spf13/cobra"
 )
 
@@ -37,7 +39,10 @@ const (
 	LONGEST_LENGTH  = 20
 )
 
-const LENGTH = "length"
+const (
+	LENGTH   = "length"
+	DATE_PIN = "date-pin"
+)
 
 type lengthFlag struct {
 	Value int
@@ -74,38 +79,62 @@ func CreateNumericCmd() *cobra.Command {
 	You can specify the length of the numeric string using the --length flag.
 	You get 4 digits by default.
 	`,
-		Run: func(cmd *cobra.Command, args []string) {
+		RunE: func(cmd *cobra.Command, args []string) error {
 
-			generateSecureNDigitNumber := func(n int) int {
+			var number int
 
-				// intPow computes integer exponentiation (10^n)
-				intPow := func(base, exp int) int {
-					result := 1
-					for i := 0; i < exp; i++ {
-						result *= base
-					}
-					return result
-				}
+			datePin, error := cmd.Flags().GetBool(DATE_PIN)
 
-				if n <= 0 {
-					return 0
-				}
+			if error != nil {
 
-				// Define the range for n-digit numbers
-				min := intPow(10, n-1)   // Smallest n-digit number
-				max := intPow(10, n) - 1 // Largest n-digit number
-
-				// Generate a cryptographically secure random number
-				num, _ := rand.Int(rand.Reader, big.NewInt(int64(max-min+1)))
-				return int(num.Int64()) + min
+				return custom_errors.CreateInvalidFlagErrorWithMessage(
+					custom_errors.FlagName(DATE_PIN),
+					error.Error(),
+				)
 			}
 
-			printer.PrintUsingCommmand(cmd, fmt.Sprintf("%d", generateSecureNDigitNumber(lengthFlag.Value)))
+			if datePin {
+				number = int(time.Now().UnixMilli())
+			} else {
+
+				generateSecureNDigitNumber := func(n int) int {
+
+					// intPow computes integer exponentiation (10^n)
+					intPow := func(base, exp int) int {
+						result := 1
+						for i := 0; i < exp; i++ {
+							result *= base
+						}
+						return result
+					}
+
+					if n <= 0 {
+						return 0
+					}
+
+					// Define the range for n-digit numbers
+					min := intPow(10, n-1)   // Smallest n-digit number
+					max := intPow(10, n) - 1 // Largest n-digit number
+
+					// Generate a cryptographically secure random number
+					num, _ := rand.Int(rand.Reader, big.NewInt(int64(max-min+1)))
+					return int(num.Int64()) + min
+				}
+
+				number = generateSecureNDigitNumber(lengthFlag.Value)
+			}
+
+			return printer.PrintUsingCommmand(cmd, fmt.Sprintf("%d", number))
 
 		},
 	}
 
 	numericCmd.Flags().VarP(&lengthFlag, LENGTH, "l", "Length of the numeric string")
+
+	numericCmd.Flags().BoolP(DATE_PIN, "d", false, "Create a numeric string using a timestamp")
+
+	numericCmd.MarkFlagsMutuallyExclusive(LENGTH, DATE_PIN)
+
 	return numericCmd
 }
 
